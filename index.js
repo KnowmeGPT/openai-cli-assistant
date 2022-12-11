e3cc0213fba3963f567d4997f5c3c99d02348d4c
KnowmeGPT <135098949+KnowmeGPT@users.noreply.github.com>
2022-12-11 22:25:02 +0000

    Upgrade dependencies.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 import { createInterface } from "node:readline/promises";
 import { stdin as input, stdout as output, env } from "node:process";
-import { Configuration, OpenAIApi }
+import { Configuration, OpenAIApi } from "op
