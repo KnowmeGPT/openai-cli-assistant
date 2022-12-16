32059e68c5f6ddb6e884cba52b9a101a5ece87a8
KnowmeGPT <135098949+KnowmeGPT@users.noreply.github.com>
2022-12-16 07:50:34 +0000

    Removed stray console.log

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import { createInterface } from "node:readline/promises";
 import { stdin as input, stdout as output, env } from "node:process";
 import { Configuration, OpenAIApi } from "openai";
 
-const configuration = new Configuration({ apiKey: env.OPENAI_API_KE
+const configuration = new Configuration({ apiKey: env.OPENAI_API_KEY });
+co
