fb2e74257b8fd3f0c30a08dbd5cdb992c995c5dc
KnowmeGPT <135098949+KnowmeGPT@users.noreply.github.com>
2022-12-19 12:26:21 +0000

    Pontoon: Update Albanian (sq) localization of AMO

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({ apiKey: env.OPENAI_API_KEY });
 const openai = new OpenAIApi(configuration);
-co
+const rea
