58c58f02dc5a142479f1a40a15c9123e433d8be2
KnowmeGPT <135098949+KnowmeGPT@users.noreply.github.com>
2022-12-22 19:16:10 +0000

    fix rspec

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const configuration = new Configuration({ apiKey: env.OPENAI_API_KEY });
 const openai = new OpenAIApi(configuration);
 const readline = createInterface({ input, output });
 
-const chatbo
+const chatbotType =
