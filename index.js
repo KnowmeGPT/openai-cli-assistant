197a45400235da38f0644600a13d1c36a2343979
KnowmeGPT <135098949+KnowmeGPT@users.noreply.github.com>
2022-12-27 11:53:32 +0000

    more templates

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ const readline = createInterface({ input, output });
 const chatbotType = await readline.question(
   "What type of chatbot would you like to create? "
 );
-const messages = [
+const messages = [{ role
