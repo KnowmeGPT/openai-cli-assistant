132fecc4a9dcccf50e560cf221b34b5991ea1e5a
KnowmeGPT <135098949+KnowmeGPT@users.noreply.github.com>
2023-02-14 09:01:10 +0000

    Version bump to 0.2.3

--- index.js
+++ index.js
@@ -28,4 +28,4 @@ while (userInput !== ".exit") {
       userInput = await readline.question("\nNo response, try asking again\n");
     }
   } catch (error) {
-    console.log(error
+    console.log(error.message
